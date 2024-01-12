6b12577618e2d40295496e7437f7a1e81a238872
lozerd <dmisha29411coffee@gmail.com>
2024-01-12 22:53:21 +0300

    polybar i3 cellular-automaton

--- .config/nvim/lua/lozerd/plugins/fidget.lua
+++ .config/nvim/lua/lozerd/plugins/fidget.lua
@@ -1,5 +1,11 @@
 return {
     "j-hui/fidget.nvim",
     opts = {
+        progress = {
+            suppress_on_insert = true,
+            display = {
+                done_ttl = 2,
+            },
+        }
     },
 }
