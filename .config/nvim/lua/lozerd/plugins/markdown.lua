2d8ffdd41285e4269714eb8b2e57a1c75fbfb332
Lozerd <dmisha29411@gmail.com>
2024-11-27 09:44:40 +0300

    fix(nvim): merge conflict resolved

--- .config/nvim/lua/lozerd/plugins/markdown.lua
+++ .config/nvim/lua/lozerd/plugins/markdown.lua
@@ -1,10 +1,5 @@
 return {
-<<<<<<< HEAD
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-=======
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
->>>>>>> ac52524 (feat(hyprland, nvim): update hypr conf, neovim new plugins)
     opts = {},
 }
