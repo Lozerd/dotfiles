ecb5e237f5f43e3b7bb4f2ea071255cb524be490
Lozerd <dmisha29411@gmail.com>
2025-05-15 09:34:20 +0300

    feat(nvim): reimplement deprecated diagnostics

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,64 +1,60 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(client, bufnr)
-        -- stylua: ignore start
-        local opts = { buffer = bufnr, remap = false }
-        local ks = vim.keymap.set
+	callback = function(args)
+		local opts = { buffer = args.buf, remap = false }
+		local ks = vim.keymap.set
 
-        vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+		vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+        -- stylua: ignore start
         ks("n", "gd", function() vim.lsp.buf.definition() end, opts)
         ks("n", "gD", function() vim.lsp.buf.declaration() end, opts)
         ks("n", "K", function() vim.lsp.buf.hover() end, opts)
         ks("n", "<leader>vws", function(s) vim.lsp.buf.workspace_symbol(s) end, opts)
         ks("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        ks("n", "[d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
-        ks("n", "]d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
+        ks("n", "[d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
+        ks("n", "]d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
         ks("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         ks("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         ks("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         ks({ "n", "v" }, "<leader>r", function() vim.lsp.buf.rename() end, opts)
-        ks(
-            "n",
-            "<M-CR>",
-            function()
-                vim.lsp.buf.code_action({ context = { only = { "quickfix" } } })
-            end,
-            opts
-        )
-        ks(
-            "n",
-            "<C-A-O>",
-            function()
-                local filter = function(action) return action.kind == "source.organizeImports" end
-                vim.lsp.buf.code_action({ filter = filter, apply = true })
-            end,
-            opts
-        )
-
-        -- git-blame
-        vim.keymap.set("n", "<leader>gbo", "<cmd>GitBlameOpenCommitURL<CR>", { buffer = bufnr, remap = false })
 		-- stylua: ignore end
+		ks("n", "<M-CR>", function()
+			vim.lsp.buf.code_action({ context = { only = { "quickfix" } } })
+		end, opts)
+		ks("n", "<C-A-O>", function()
+			local filter = function(action)
+				return action.kind == "source.organizeImports"
+			end
+			vim.lsp.buf.code_action({ filter = filter, apply = true })
+		end, opts)
+
+		-- git-blame
+		vim.keymap.set("n", "<leader>gbo", "<cmd>GitBlameOpenCommitURL<CR>", opts)
 	end,
 })
 
-local signs = { Error = " ", Warn = " ", Hint = "󰌶", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+if vim.fn.has("nvim-0.10") ~= 0 then
+	local signs = { Error = " ", Warn = " ", Hint = "󰌶", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	update_in_insert = false,
-	virtual_text = { spacing = 4, prefix = "\u{ea71}" },
-	severity_sort = true,
-})
+local severity = vim.diagnostic.severity
 
 vim.diagnostic.config({
-	virtual_text = {
-		prefix = "●",
-	},
+	underline = true,
+	virtual_text = { prefix = "●" },
+	-- virtual_text = { spacing = 4, prefix = "\u{ea71}" },
 	update_in_insert = true,
-	float = { source = "always" },
+	float = { source = false },
+	severity_sort = true,
+	signs = {
+		[severity.ERROR] = " ",
+		[severity.WARN] = " ",
+		[severity.HINT] = "󰌶",
+		[severity.INFO] = " ",
+	},
 })
 
 vim.lsp.config("lua_ls", {
